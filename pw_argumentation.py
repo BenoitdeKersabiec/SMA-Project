38aa6b9b429725071b68960c03a7b72d6817d5f4
Maszewski Timo <timo.maszewski@student-cs.fr>
2022-04-01 11:20:17 +0200

    fusion

--- pw_argumentation.py
+++ pw_argumentation.py
@@ -11,6 +11,7 @@ from communication.message.MessageService import MessageService
 from communication.message.MessagePerformative import MessagePerformative
 from communication.preferences.Preferences import Item, Preferences, CriterionName, CriterionValue
 from communication.preferences.Value import Value
+from arguments.Argument import Argument
 from random import shuffle
 
 
@@ -24,23 +25,50 @@ class ArgumentAgent(CommunicatingAgent):
 
     def step(self):
         super().step()
-        inbox = self.receive_message()
-        for in_message in inbox:
-            if in_message.get_performative().name == "PROPOSE":
-                if self.preference.is_item_among_top_10_percent(received[-1].get_content(), self.model.item_list):
-                    out_respond = Message(self.get_name(), in_message.get_exp(), MessagePerformative(102), in_message.get_content())
-                    print(self.get_name(), "sending message:")
-                    print(out_respond)
-                    self.send_message(out_respond)
-                    self.state="Accepted"
+        inbox = self.get_new_messages()
+        # for in_message in inbox:
+        #     if in_message.get_performative().name == "PROPOSE":
+        #         if self.preference.is_item_among_top_10_percent(received[-1].get_content(), self.model.item_list):
+        #             out_respond = Message(self.get_name(), in_message.get_exp(), MessagePerformative(102), in_message.get_content())
+        #             print(self.get_name(), "sending message:")
+        #             print(out_respond)
+        #             self.send_message(out_respond)
+        #             self.state="Accepted"
+        #         else:
+        #             out_respond = Message(self.get_name(), in_message.get_exp(), MessagePerformative(104), in_message.get_content())
+        #             print(out_respond)
+        #             self.send_message(out_respond)
+        #     if in_message.get_performative().name == "COMMIT" and self.state == "Accepted":
+        #         out_respond = Message(self.get_name(), in_message.get_exp(), MessagePerformative(103), in_message.get_content())
+        #         print(out_respond)
+        #         self.send_message(out_respond)
+        for received in inbox:
+            print(received)
+            exp = received.get_exp()
+            perf = received.get_performative()
+            item = received.get_content()
+            if perf==MessagePerformative.PROPOSE:
+                if self.preference.is_item_among_top_10_percent(item, self.model.item_list):
+                    respond = Message(self.get_name(), exp, MessagePerformative.ACCEPT, item)
+                    self.send_message(respond)
                 else:
-                    out_respond = Message(self.get_name(), in_message.get_exp(), MessagePerformative(104), in_message.get_content())
-                    print(out_respond)
-                    self.send_message(out_respond)
-            if in_message.get_performative().name == "COMMIT" and self.state == "Accepted":
-                out_respond = Message(self.get_name(), in_message.get_exp(), MessagePerformative(103), in_message.get_content())
-                print(out_respond)
-                self.send_message(out_respond)
+                    respond = Message(self.get_name(), exp, MessagePerformative.ASK_WHY, item)
+                    self.send_message(respond)
+            elif perf==MessagePerformative.ACCEPT:
+                respond = Message(self.get_name(), exp, MessagePerformative.COMMIT, item)
+                self.send_message(respond)
+            elif perf==MessagePerformative.COMMIT:
+                previous_messages = self.get_messages_from_exp(exp)
+                if len(previous_messages)>1 and previous_messages[-2].get_performative()==MessagePerformative.ACCEPT and item not in self.model.accepted_items:
+                    self.model.accepted_items.append(item)
+                else:
+                    respond = Message(self.get_name(), exp, MessagePerformative.COMMIT, item)
+                    self.send_message(respond)
+            elif perf==MessagePerformative.ASK_WHY:
+                arg = self.support_proposal(item)
+                print(arg)
+                respond = Message(self.get_name(), exp, MessagePerformative.ARGUE, item)
+                self.send_message(respond)
 
 
 
@@ -69,6 +97,20 @@ class ArgumentAgent(CommunicatingAgent):
         shuffle(values_list)
         return values_list[0]
 
+    def support_proposal(self, item): 
+        """  
+        Used when the agent receives "ASK_WHY" after having proposed an item 
+        :param item: str- name of the item which was proposed 
+        :return: string- the strongest supportive argument 
+        """
+        argument = Argument(boolean_decision=True, item=item)
+        list_supporting_proposal = argument.List_supporting_proposal(item=item, preferences = self.get_preference())
+        if len(list_supporting_proposal) == 0:
+            print("No argument to support the item")
+            best_argument = None
+        else:
+            best_argument = list_supporting_proposal[0]
+        return best_argument
 
 
 
@@ -99,35 +141,6 @@ class ArgumentModel(Model):
         self.__messages_service.dispatch_messages(self.A1, self.A2, )
         self.schedule.step()
 
-def interact(agent, model):
-    new_messages=agent.get_new_messages()
-    for message in new_messages:
-        received = message
-        print(received)
-        exp = received.get_exp()
-        perf = received.get_performative()
-        item = received.get_content()
-        if perf==MessagePerformative.PROPOSE:
-            if agent.preference.is_item_among_top_10_percent(item, model.item_list):
-                respond = Message(agent.get_name(), exp, MessagePerformative.ACCEPT, item)
-                agent.send_message(respond)
-            else:
-                respond = Message(agent.get_name(), exp, MessagePerformative.ASK_WHY, item)
-                agent.send_message(respond)
-        elif perf==MessagePerformative.ACCEPT:
-            respond = Message(agent.get_name(), exp, MessagePerformative.COMMIT, item)
-            agent.send_message(respond)
-        elif perf==MessagePerformative.COMMIT:
-            previous_messages = agent.get_messages_from_exp(exp)
-            if len(previous_messages)>1 and previous_messages[-2].get_performative()==MessagePerformative.ACCEPT and item not in model.accepted_items:
-                model.accepted_items.append(item)
-            else:
-                respond = Message(agent.get_name(), exp, MessagePerformative.COMMIT, item)
-                agent.send_message(respond)
-
-
-
-
 
 
 if __name__ == "__main__":
@@ -153,5 +166,7 @@ if __name__ == "__main__":
     while steps>0:
         steps-=1
         for agent in argument_model.schedule.agents:
-            interact(agent, argument_model)
+            agent.step()
+
+    
 
